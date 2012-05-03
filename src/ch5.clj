fe32533ae9610371880906057dd114595b8c7643
Carl Thuringer <carl@groupon.com>
2012-05-02 20:55:14 -0500

    Got some describe-paths working

--- src/ch5.clj
+++ src/ch5.clj
@@ -1,9 +1,31 @@
-(ns ch5)
+(ns ch5
+  (use [clojure.string :only (join)]))
+
 
 (def ^:dynamic *nodes*
-  '((living-room (you are in the living-room.
-                 A wizard is snoring loudly on the couch.))
-    (garden (You are in a beautiful garden.
-                 There is a well in front of you.))
-    (attic (You are in the attic.
-                There is a giant welding torch in the corner.))))
+  {:living-room "you are in the living-room. a wizard is snoring loudly on the couch."
+   :garden      "you are in a beautiful garden. a here is a well in front of you."
+   :attic       "you are in the attic. there is a giant welding torch in the corner."})
+
+(def ^:dynamic *edges*
+  {:living-room ['[garden west door]
+                '[attic upstairs ladder]]
+   :garden ['[living-room east door]]
+   :attic ['[living-room downstairs ladder]]})
+
+(defn describe-location
+  "Describe the location in the node map. Really it's just
+  an alias for 'get'."
+  [location nodes]
+  (get location nodes))
+
+(defn describe-path
+  "Describes a path from the edges map."
+  [edge]
+  (str "there is a " (last edge) " going " (second edge) " from here."))
+
+
+(defn describe-paths
+  "Describe all paths from a location given an edges map"
+  [location edges]
+  (join " " (map describe-path (get *edges* :living-room))))
--- test/land_of_lisp/test/ch5.clj
+++ test/land_of_lisp/test/ch5.clj
@@ -3,3 +3,13 @@
   (:use [expectations]))
 
 (expect 3 (count *nodes*))
+(expect "you are in the living-room. a wizard is snoring loudly on the couch."
+        (get *nodes* :living-room))
+(expect "you are in the living-room. a wizard is snoring loudly on the couch."
+        (describe-location *nodes* :living-room))
+(expect "there is a door going west from here."
+        (describe-path '(garden west door)))
+(expect "there is a door going east from here."
+        (describe-path '(living-room east door)))
+(expect "there is a door going west from here. there is a ladder going upstairs from here."
+        (describe-paths 'living-room *edges*))
